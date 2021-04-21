1e8fce9350e43f2037f2b2f2e05d81318ee11920
varunmehta1986 <mehta.varun.vm@gmail.com>
2021-04-21 23:08:53 +1000

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,37 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">&#169; vmpics.com.au</div>);
+      <div className="footer">
+        <div className="col-lg-12">
+          &#169; 2021 VMPICS PHOTOGRAPHY - YOUR FAMILY PHOTOGRAPHER
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-6" style={{ textAlign: "left" }}>
+          <table>
+            <tr>
+              <th>
+                CONTACT US
+              </th>
+            </tr>
+            <tr>
+              <td>
+                Email:
+              </td>
+              <td>
+                vmpicsphotography@gmail.com
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Phone:
+              </td>
+              <td>
+                +61-460628079
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>);
+
   }
 }
 
