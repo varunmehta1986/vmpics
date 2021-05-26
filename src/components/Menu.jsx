f57e8c6552842ac778c3b261a7f172e3a9546342
varunmehta1986 <mehta.varun.vm@gmail.com>
2021-05-26 20:41:40 +1000

    css fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,7 +14,7 @@ class Menu extends Component {
           <Navbar.Toggle />
           <Navbar.Collapse>
             <Nav>
-              <Nav.Link href="/about">About</Nav.Link>
+              {/* <Nav.Link href="/about">About</Nav.Link> */}
               <Nav.Link href="/Family">Family Photos</Nav.Link>
               <Nav.Link href="/maternity">Maternity Photos</Nav.Link>
               <Nav.Link href="/contact">Contact</Nav.Link>
