171b778c1025e3ea2852e4852d36dc6f1c1b2359
Varun Mehta <varunmehta@IITLAPTOP277-Varun-Mehta.local>
2021-07-17 23:21:34 +1000

    3rd package

--- src/components/PrintPackages.jsx
+++ src/components/PrintPackages.jsx
@@ -80,7 +80,7 @@ class PrintPackages extends Component{
       
                         </Carousel>
                         <Card.Body>
-                            <Card.Title>Canvas Prints</Card.Title>
+                            <Card.Title>Canvas Prints Plus</Card.Title>
                             <Card.Text>
                                 <ul>
                                     <li>
@@ -100,6 +100,54 @@ class PrintPackages extends Component{
                                     </li>
                                     <li>
                                         2 90cms x 60cms prints and 1 60cmsx40cms prints
+                                    </li>
+                                </ul>
+
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                             <Nav.Link className="btn btn-dark" style={{
+                                        margin: "1px", display: "inline-block",
+                                        padding: "6px", width: "176px"
+                                    }}
+                                    href="/contact">Book a session</Nav.Link>
+                            </Card.Footer>
+                    </Card>
+                    <br />
+                    <Card style = {{maxWidth:"700px"}}>
+                        <Carousel>
+                            <Carousel.Item>
+                                <Card.Img variant="top" src={require("../images/4x75x50.jpg")}></Card.Img>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Card.Img variant="top" src={require("../images/3x90x60.jpg")}></Card.Img>
+                            </Carousel.Item>
+                            {/* <Carousel.Item>
+                                <Card.Img variant="top" src={require("../images/2x90x60 1x60x40.jpg")}></Card.Img>
+                            </Carousel.Item> */}
+      
+                        </Carousel>
+                        <Card.Body>
+                            <Card.Title>Canvas Prints Max</Card.Title>
+                            <Card.Text>
+                                <ul>
+                                    <li>
+                                        Price : $500
+                                    </li>
+                                    <li>
+                                        Package includes any 60+ minutes maternity or family session 
+                                    </li>
+                                    <li>
+                                        20 digital edited images
+                                    </li>
+                                    <li>
+                                        2 120cms x 80cms prints OR
+                                    </li>
+                                    <li>
+                                        3 90cms x 60cms prints OR
+                                    </li>
+                                    <li>
+                                        4 75cms x 50cms prints
                                     </li>
                                 </ul>
 
