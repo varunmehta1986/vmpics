9bdeeb732a676a9c9c06125ab1624e9770437de2
varunmehta1986 <mehta.varun.vm@gmail.com>
2021-07-22 22:27:31 +1000

    H1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,10 @@ class Home extends Component {
       <div>
         <div className="homeImage">
           <Menu variant="dark"></Menu>
-          {/* <div className="col-lg-4 text-left .d-none .d-sm-block"
-            style={{ fontSize: "55px", color: "#ffe8cc"}}>
-            Family Photographer based in Sydney, Australia
-          </div> */}
+          <div className="col-lg-4 text- .d-none .d-sm-block"
+            style={{ color: "#ffe8cc" }}>
+            <h1>Family Photographer based in Sydney, Australia</h1>
+          </div>
         </div >
         <br />
         <About />
