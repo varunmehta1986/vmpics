2de8890d5389c814d5eb3af63cd07ecc489158c7
varunmehta1986 <mehta.varun.vm@gmail.com>
2022-03-07 23:12:07 +1100

    Home page carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import About from "./About";
 import Menu from './Menu';
 import Reviews from './Reviews';
 import Helmet from 'react-helmet'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+var Carousel = require('react-responsive-carousel').Carousel;
 
 class Home extends Component {
   render() {
@@ -12,18 +14,69 @@ class Home extends Component {
           <title>Welcome to VMPics Photography. </title>
           <meta name="description" content='Family photographer in Hills District of NSW. Australia'></meta>
         </Helmet>
-        <div className="homeImage">
-          <Menu variant="dark"></Menu>
-          <div className="col-lg-4 text- .d-none .d-sm-block"
-            style={{ color: "#ffe8cc" }}>
-            <h1>Family Photographer based in Sydney, Australia</h1>
-          </div>
+        <div style={{ textAlign: 'center' }} autoPlay interval="3000" transitionTime="3000">
+          <Menu></Menu>
+          <Carousel showArrows={true}>
+            <div>
+              <img src={require("../images/Carousel/Carousel1.jpg")} alt='Carousel 1' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel2.jpg")} alt='Carousel 2' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel3.jpg")} alt='Carousel 3' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel4.jpg")} alt='Carousel 4' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel5.jpg")} alt='Carousel 5' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel6.jpg")} alt='Carousel 6' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel7.jpg")} alt='Carousel 7' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel8.jpg")} alt='Carousel 8' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel9.jpg")} alt='Carousel 9' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel10.jpg")} alt='Carousel 10' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel11.jpg")} alt='Carousel 11' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel12.jpg")} alt='Carousel 12' />
+
+            </div>
+            <div>
+              <img src={require("../images/Carousel/Carousel13.jpg")} alt='Carousel 13' />
+
+            </div>
+
+          </Carousel>
         </div >
         <br />
         <About />
         <br />
         <Reviews />
-      </div>
+      </div >
     );
   }
 }
