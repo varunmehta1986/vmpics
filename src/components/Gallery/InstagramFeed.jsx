19603baff739ee42068b94e9a12fde5e4f93033c
varunmehta1986 <mehta.varun.vm@gmail.com>
2022-05-07 22:14:30 +1000

    fix feed

--- src/components/Gallery/InstagramFeed.jsx
+++ src/components/Gallery/InstagramFeed.jsx
@@ -7,10 +7,9 @@ class InstagramFeed extends Component {
                 <div>
                     <div class="col-12">
                         <h4>LATEST FROM INSTAGRAM</h4>
-                        <div class="tagembed-container" style={{ width: '96%', height: '300px', overflow: 'auto' }}>
+                        <div class="tagembed-container" style={{ width: "100%", height: "100%", overflow: "auto" }}>
                             <div class="tagembed-socialwall" data-wall-id="37711" view-url="https://widget.tagembed.com/37711?view">
-                            </div>
-                        </div>
+                            </div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
                     </div>
                 </div>
             </div>
