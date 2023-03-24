0a033d095929ca26330dc80fd9975418b02bc3c8
varunmehta1986 <mehta.varun.vm@gmail.com>
2023-03-24 11:59:55 +1100

    shuffle

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -96,68 +96,23 @@ class HomeCarousel extends Component {
     ];
 
     render() {
+        this.shufflePhotos();
+
         return (
             <Gallery photos={this.photos} />
-
-            // <div style={{ textAlign: 'center' }}>
-            //     <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} interval={5000} transitionTime={2000}>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel1.jpg")} alt='Carousel 1' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel2.jpg")} alt='Carousel 2' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel3.jpg")} alt='Carousel 3' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel4.jpg")} alt='Carousel 4' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel5.jpg")} alt='Carousel 5' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel6.jpg")} alt='Carousel 6' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel7.jpg")} alt='Carousel 7' />
-
-            //         </div>
-            //         <div>
-            //             <img src={} alt='Carousel 8' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel9.jpg")} alt='Carousel 9' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel10.jpg")} alt='Carousel 10' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel11.jpg")} alt='Carousel 11' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel12.jpg")} alt='Carousel 12' />
-
-            //         </div>
-            //         <div>
-            //             <img src={require("../images/Carousel/Carousel13.jpg")} alt='Carousel 13' />
-
-            //         </div>
-            //     </Carousel>
-            // </div >
-
         );
     }
+
+    shufflePhotos() {
+
+        for (let i = this.photos.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.photos[i];
+            this.photos[i] = this.photos[j];
+            this.photos[j] = temp;
+        }
+    }
+
 }
 
 export default HomeCarousel;
